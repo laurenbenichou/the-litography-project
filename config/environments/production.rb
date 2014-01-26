220fcb11ee45a6f7facf1a678dec66aae7eee092
Michael Johnston <michaelajohnston@mac.com>
2014-01-26 02:54:10 -0800

    precompiling ckeditor to see if it fixes 404

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Thelitographyproject::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.precompile += Ckeditor.assets
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
