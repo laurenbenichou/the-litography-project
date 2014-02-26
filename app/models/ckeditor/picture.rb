25a5be2e1339c61e47395bcaafa6b45bd59f3253
Michael Johnston <michaelajohnston@mac.com>
2014-02-25 23:41:01 -0800

    not sure

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -8,7 +8,7 @@ class Ckeditor::Picture < Ckeditor::Asset
     validates_attachment_size :data, :less_than => 2.megabytes
     validates_attachment_presence :data
   rescue
-    puts "exception in Ckeditor attachment file"
+    puts "exception in Ckeditor attachment file" 
   end
 
   def url_content
